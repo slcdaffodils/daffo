17d19514f6562993d64aaf6ab7adcf753bc01104
yogeshdakre <yogeshdakre@gmail.com>
2017-03-08 21:24:31 +0530

    first commit

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
+ * more info on how to use sw-toolbox to custom configure your service worker.
+ */
+
+
+'use strict';
+importScripts('./build/sw-toolbox.js');
+
+self.toolbox.options.cache = {
+  name: 'ionic-cache'
+};
+
+// pre-cache our key assets
+self.toolbox.precache(
+  [
+    './build/main.js',
+    './build/main.css',
+     './assets/animate.css',
+     './assets/ng2-toastr.min.css',
+      './assets/Chart.js',
+      './assets/aag1.gif',
+    './build/polyfills.js',
+    'cordova.js',
+    'cordova_plugins.js',
+    'index.html',
+    'manifest.json'
+  ]
+);
+
+// dynamically cache any other local assets
+self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+
+// for any other requests go to the network, cache,
+// and then only use that cached resource if your user goes offline
+self.toolbox.router.default = self.toolbox.networkFirst;

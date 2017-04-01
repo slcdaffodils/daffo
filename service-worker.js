9cb6f412504c122c95d56e6491e1888b91b38135
yogeshdakre <yogeshdakre@gmail.com>
2017-04-01 12:09:00 +0530

    first commit

--- service-worker.js
+++ service-worker.js
@@ -14,13 +14,13 @@ self.toolbox.options.cache = {
 // pre-cache our key assets
 self.toolbox.precache(
   [
-    './build/main.js',
+    './build/main1234.js',
     './build/main.css',
      './assets/animate.css',
      './assets/ng2-toastr.min.css',
       './assets/Chart.js',
       './assets/aag1.gif',
-    './build/polyfills.js',
+    './build/polyfills1234.js',
     'cordova.js',
     'cordova_plugins.js',
     'index.html',

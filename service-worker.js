df5ffda8d2ada1bb2f5224fa2242baffdae8991e
yogeshdakre <yogeshdakre@gmail.com>
2017-08-06 11:45:24 +0530

    app off

--- service-worker.js
+++ service-worker.js
@@ -14,13 +14,13 @@ self.toolbox.options.cache = {
 // pre-cache our key assets
 self.toolbox.precache(
   [
-    './build/yogi0608172.js',
+    './build/yogi0608173.js',
     './build/main.css',
      './assets/animate.css',
      './assets/ng2-toastr.min.css',
       './assets/Chart.js',
       './assets/aag.gif',
-    './build/yogifills0608172.js',
+    './build/yogifills0608173.js',
     'cordova.js',
     'cordova_plugins.js',
     'index.html',

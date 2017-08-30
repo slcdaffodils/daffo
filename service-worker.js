e0dadbae3fc4c938013dd91ea6a718c093bb5d71
yogeshdakre <yogeshdakre@gmail.com>
2017-08-30 23:38:43 +0530

    remove webindex

--- service-worker.js
+++ service-worker.js
@@ -14,13 +14,13 @@ self.toolbox.options.cache = {
 // pre-cache our key assets
 self.toolbox.precache(
   [
-    './build/yogi28082017.js',
-    './build/main28082017.css',
+    './build/yogi30082017.js',
+    './build/main30082017.css',
      './assets/animate.css',
      './assets/ng2-toastr.min.css',
       './assets/Chart.js',
       './assets/aag.gif',
-    './build/yogifills28082017.js',
+    './build/yogifills30082017.js',
     'cordova.js',
     'cordova_plugins.js',
     'index.html',

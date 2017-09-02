59c68f1798779569e4de25e349122f2fb7537e18
yogeshdakre <yogeshdakre@gmail.com>
2017-09-02 16:56:09 +0530

    data

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ self.toolbox.options.cache = {
 self.toolbox.precache(
   [
     './build/yogi30082017.js',
-    './build/mainx.css',
+    './build/main28082017.css',
      './assets/animate.css',
      './assets/ng2-toastr.min.css',
       './assets/Chart.js',
